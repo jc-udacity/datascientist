5547486d30faa882205918d691120f7f20504ff8
JC <piersjea+udacity@gmail.com>
2019-03-15 16:36:12 +0100

    data science update

--- 05_data_science/01_data_science_process/ALookAtTheDataSolns.py
+++ 05_data_science/01_data_science_process/ALookAtTheDataSolns.py
@@ -1,9 +1,5 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score, mean_squared_error
-
 
 df = pd.read_csv('./survey_results_public.csv')
 schema = pd.read_csv('./survey_results_schema.csv')
